dfd7fcb9c88e369e0ebae243ad550a9d6e33e180
Bruna Gomes <heathjared2@gmail.com>
2021-12-10 07:57:46 -0300

    atualização do serviço de log

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app)
 
 # app.wsgi_app = ProxyFix(app.wsgi_app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://USER:PASSWORD@localhost:PORT/DATABASE'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:fatec@localhost:3306/calculadoradb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
